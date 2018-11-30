17aeec6b64e7673b97aaeef460ef58b632752966
mayacong <459291866@qq.com>
2018-11-30 14:39:34 +0800

    增加导入应收账款的功能

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ import pymysql.cursors
 GL.LOG = getLogger('WanboLoger', 'logs', 'console.log')
 db = DB(db='wanbo')
 
+def TestDB():
+    sql = 'select * from test'
+    print(db.query(sql))
+
 def InsertToBalance(mp):
     keys_sql = '('
     values_sql = '('
@@ -22,15 +26,27 @@ def InsertToBalance(mp):
     values_sql = values_sql.rstrip(',')
     keys_sql += ')'
     values_sql += ')'
-    #sql = 'insert into balance %s values%s' % (tuple(mp.keys()), tuple(mp.values()))
     sql = 'insert into balance %s values%s' % (keys_sql,values_sql)
-    #print(sql)
-    #sql = sql.replace('None', 'NULL')
     db.exec(sql)
-
-def TestDB():
-    sql = 'select * from test'
-    print(db.query(sql))
+    
+def InsertToAccount(mp):
+    keys_sql = '('
+    values_sql = '('
+    for k,v in mp.items():
+        keys_sql += '%s,' % k
+        if v==None or v=='-' or v=='/':
+            values_sql += 'NULL,'
+        elif isinstance(v, str):
+            values_sql += '\'%s\',' % v
+        else:
+            values_sql += '%s,' % v
+    keys_sql = keys_sql.rstrip(',')
+    values_sql = values_sql.rstrip(',')
+    keys_sql += ')'
+    values_sql += ')'
+    sql = 'insert into account %s values%s' % (keys_sql,values_sql)
+    #print(sql)
+    db.exec(sql)
 
 def ReadBalanceData(source):
     n = 0
@@ -61,18 +77,57 @@ def ReadBalanceData(source):
         mp['source'] = source
         InsertToBalance(mp)
 
+def ReadAccountData():
+    n = 0
+    mp = {}
+    for row in ws.rows:
+        n = n + 1
+        if n <= 3:
+            continue
+        if n == 20:
+            break
+        if len(row) < 18:
+            return
+        mp['no'] = row[0].value
+        year = row[1].value
+        if isinstance(mp['no'],int)==False or isinstance(year,int)==False:
+            continue
+        del mp['no']
+        month = row[2].value
+        day = row[3].value
+        mp['date'] = '%d-%d-%d' % (year,month,day)
+        mp['seller'] = row[4].value
+        mp['contract'] = row[5].value
+        mp['client'] = row[6].value
+        mp['amount'] = row[7].value
+        mp['paid'] = row[8].value
+        mp['unpaid'] = row[9].value
+        mp['debt'] = row[10].value
+        mp['percent'] = row[11].value
+        mp['invoice_type'] = row[12].value
+        mp['invoice_paid'] = row[13].value
+        mp['invoice_unpaid'] = row[14].value
+        mp['status'] = row[15].value
+        mp['unpaid_reason'] = row[16].value
+        mp['commission'] = row[17].value
+        mp['commission_date'] = row[18].value
+        mp['remark'] = row[19].value
+        InsertToAccount(mp)
+
 fn = '../../db/wanbo/2018万泊财务汇总表（46周）.xlsx'
 wb = load_workbook(fn, read_only=True, data_only=True)
 for ws in wb:
-    if ws.title == '银行明细账':
-        source = '建设银行（基本户）'
-        ReadBalanceData(source)
-    elif ws.title == '现金账户1明细账':
-        source = '平安银行（姚洋）'
-        ReadBalanceData(source)
-    elif ws.title == '现金账户2明细账':
-        source = '平安银行（李昱平）'
-        ReadBalanceData(source)
+    if ws.title == '应收账款汇总表':
+        ReadAccountData()
+    #elif ws.title == '银行明细账':
+        #source = '建设银行（基本户）'
+        #ReadBalanceData(source)
+    #elif ws.title == '现金账户1明细账':
+        #source = '平安银行（姚洋）'
+        #ReadBalanceData(source)
+    #elif ws.title == '现金账户2明细账':
+        #source = '平安银行（李昱平）'
+        #ReadBalanceData(source)
     else:
         pass
 
