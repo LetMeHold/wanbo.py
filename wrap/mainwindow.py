9704549ed873cd78f87ced6c875c001eb5605e32
mayacong <459291866@qq.com>
2018-12-06 11:21:13 +0800

    调整代码结构

--- wrap/mainwindow.py
+++ wrap/mainwindow.py
@@ -21,13 +21,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         GL.LOG.info('主界面关闭')
 
     def init(self):
-        self.tableData = None
-        self.tableHeads = None
-
-        self.twData.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.twData.customContextMenuRequested.connect(self.tablePopMenu)
-        self.twData.horizontalHeader().setStyleSheet("QHeaderView::section{background:skyblue;}")
-        self.twData.verticalHeader().setStyleSheet("QHeaderView::section{background:skyblue;}")
+        self.twQuery.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.twQuery.customContextMenuRequested.connect(self.tablePopMenu)
+        self.twQuery.horizontalHeader().setStyleSheet("QHeaderView::section{background:skyblue;}")
+        self.twQuery.verticalHeader().setStyleSheet("QHeaderView::section{background:skyblue;}")
 
         self.cmbTable.addItem('无')
         self.cmbTable.addItems(list(self.bus.tables().keys()))
@@ -43,20 +40,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.btnRefresh.clicked.connect(self.btnRefreshClicked)
         self.edtFilter.textChanged.connect(self.edtFilterChanged)
         self.btnAdd.clicked.connect(self.btnAddClicked)
-        self.twData.itemDoubleClicked.connect(self.dataItemEdit)
-
-    def dataItemEdit(self, it):
-        self.twData.itemChanged.connect(self.dataItemChanged)
+        self.twQuery.itemChanged.connect(self.dataItemChanged)
 
     def dataItemChanged(self, it):
-        self.twData.itemChanged.disconnect()
         r = it.row()
         c = it.column()
-        id_it = self.twData.item(r, 0)
-        id_enHead = self.tableHeads[0][0]
+        id_it = self.twQuery.item(r, 0)
+        id_enHead = self.tableQueryHead[0][0]
         id_value = int(id_it.text())
-        enHead = self.tableHeads[0][c]
-        tp = self.tableHeads[2][c]
+        enHead = self.tableQueryHead[0][c]
+        tp = self.tableQueryHead[2][c]
         sql = 'update %s set %s = %TBD where %s = %d' 
         value = it.text()
         if value.strip() == '':
@@ -70,83 +63,86 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             value = float(value)
         else:
             sql = sql.replace('%TBD','"%s"', 1)
-        sql = sql % (self.table,enHead,value,id_enHead,id_value)
+        sql = sql % (self.tableQuery,enHead,value,id_enHead,id_value)
         self.bus.execSql(sql)
 
     def tablePopMenu(self, pos):
         self.popmenu.popup(QCursor.pos())
 
     def actFilterClicked(self):
-        item = self.twData.currentItem()
+        item = self.twQuery.currentItem()
         self.edtFilter.setText(item.text())
 
-    def initTable(self, table):
-        self.table = table
-        self.tableData = self.bus.selectTableData(table)
-        self.tableHeads = self.bus.selectTableHead(table)
-        self.twData.clear()
-        self.twData.setColumnCount(len(self.tableHeads[1]))
-        self.twData.setRowCount(len(self.tableData))
-        self.twData.setHorizontalHeaderLabels(self.tableHeads[1])
-        self.isAdding = False
-        self.btnAdd.setText('新增')
+    def fillTableQuery(self, table):
+        self.tableQuery = table
+        self.tableQueryData = self.bus.selectTableData(self.tableQuery)
+        self.tableQueryHead = self.bus.selectTableHead(self.tableQuery)
+        self.twQuery.itemChanged.disconnect()
+        self.fillTable(self.twQuery, self.tableQuery, self.tableQueryData, self.tableQueryHead)
+        self.twQuery.itemChanged.connect(self.dataItemChanged)
 
     def cmbTableSelected(self, index):
         key = self.cmbTable.itemText(index)
         if key in self.bus.tables():
-            self.initTable(self.bus.tables()[key])
-            self.refreshData(self.tableData, self.tableHeads)
+            self.fillTableQuery(self.bus.tables()[key])
 
     def btnRefreshClicked(self):
         key = self.cmbTable.currentText()
         if key in self.bus.tables():
-            self.initTable(self.bus.tables()[key])
-            self.refreshData(self.tableData, self.tableHeads)
+            self.fillTableQuery(self.bus.tables()[key])
 
     def edtFilterChanged(self, txt):
         if len(txt) < 2:
             return
-        current = self.twData.currentItem()
-        items = self.twData.findItems(txt, Qt.MatchContains)
+        current = self.twQuery.currentItem()
+        items = self.twQuery.findItems(txt, Qt.MatchContains)
         showRows = []
         for it in items:
             if it.row() not in showRows:
                 showRows.append(it.row())
-        self.twData.setVisible(False)
-        for i in range(0,self.twData.rowCount()):
+        self.twQuery.setVisible(False)
+        for i in range(0,self.twQuery.rowCount()):
             if i not in showRows:
-                self.twData.setRowHidden(i, True)
+                self.twQuery.setRowHidden(i, True)
             else:
-                self.twData.setRowHidden(i, False)
-        self.twData.setCurrentItem(current)
-        self.twData.setVisible(True)
+                self.twQuery.setRowHidden(i, False)
+        self.twQuery.setCurrentItem(current)
+        self.twQuery.setVisible(True)
 
-    def refreshData(self, data, heads):
-        self.twData.setVisible(False)
-        for r in range(0,self.twData.rowCount()):
-            for c in range(0,self.twData.columnCount()):
-                tmp = data[r][heads[0][c]]
+    def fillTable(self, tw, table, data, head):
+        tw.clear()
+        tw.setColumnCount(len(head[1]))
+        tw.setRowCount(len(data))
+        tw.setHorizontalHeaderLabels(head[1])
+        self.btnAdd.setText('新增')
+
+        tw.setVisible(False)
+        for r in range(0,tw.rowCount()):
+            for c in range(0,tw.columnCount()):
+                tmp = data[r][head[0][c]]
                 if tmp == None:
                     tmp = ''
                 it = QTableWidgetItem(str(tmp))
-                self.twData.setItem(r,c,it)
-                self.twData.setRowHidden(r, False)
-        self.twData.setVisible(True)
+                tw.setItem(r,c,it)
+                if c == 0:
+                    it.setFlags(it.flags() & ~Qt.ItemIsEditable)
+                tw.setRowHidden(r, False)
+        tw.setVisible(True)
 
     def btnAddClicked(self):
         key = self.cmbTable.currentText()
         if key not in self.bus.tables():
             return
         if self.btnAdd.text() == '新增':
-            self.twData.clearContents()
-            self.twData.setRowCount(1)
+            self.twQuery.clearContents()
+            self.twQuery.setRowCount(1)
             self.btnAdd.setText('确认新增')
         else:
             sql = self.bus.insertTemplates()[key]
             r = 0
             datas = []
-            for c in range(1,self.twData.columnCount()):
-                it = self.twData.item(r, c)
+            for c in range(1,self.twQuery.columnCount()):
+                it = self.twQuery.item(r, c)
                 txt = None
                 #表格为空或都是空格就以None处理
                 if it != None:
@@ -155,10 +151,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         txt = None
                 #根据DB中的类型对txt进行处理
                 if txt != None:
-                    if self.tableHeads[2][c] == 'int':
+                    if self.tableQueryHead[2][c] == 'int':
                         sql = sql.replace('%TBD','%d', 1)
                         txt = int(txt)
-                    elif self.tableHeads[2][c] == 'double':
+                    elif self.tableQueryHead[2][c] == 'double':
                         sql = sql.replace('%TBD','%.2f', 1)
                         txt = float(txt)
                     else:
